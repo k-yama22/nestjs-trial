49e3e98de3c279808f717aba6d13af29c43c5b81
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-19 17:15:55 +0900

    ✨ コントローラーにユーザー参照機能の追加

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,15 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('users')
+  get_users_proc(): { id: string; name: string; age: number }[] {
+    const users = [
+      { id: '01', name: '山田太郎', age: 24 },
+      { id: '02', name: '佐藤次郎', age: 25 },
+      { id: '03', name: '伊藤三郎', age: 26 },
+      { id: '04', name: '高橋四郎', age: 27 },
+    ];
+    return users;
+  }
 }
