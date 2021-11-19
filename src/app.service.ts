fa5a56386523e432eb853fb12136a1f3eabb325c
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-19 17:24:13 +0900

    ♻️ ユーザー参照機能の処理内容をサービスに移行

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUsers(): { id: string; name: string; age: number }[] {
+    const users = [
+      { id: '01', name: '山田太郎', age: 24 },
+      { id: '02', name: '佐藤次郎', age: 25 },
+      { id: '03', name: '伊藤三郎', age: 26 },
+      { id: '04', name: '高橋四郎', age: 27 },
+      { id: '05', name: '茂野五郎', age: 28 },
+    ];
+    return users;
+  }
 }
