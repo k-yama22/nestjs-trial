0a51b461806c0225c2660f00ddc1873c95272b6c
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-20 22:33:53 +0900

    ✨ ユーザー登録機能とバリデーションの実装

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.model';
 
 @Controller()
 export class AppController {
@@ -11,7 +20,14 @@ export class AppController {
   }
 
   @Get('users')
-  getUsers(): { id: string; name: string; age: number }[] {
+  getUsers(): { id: number; name: string; age: number }[] {
     return this.appService.getUsers();
+  }
+
+  @Post('users')
+  @UsePipes(ValidationPipe)
+  // @Bodyでrequest.bodyを全取得。
+  createTodo(@Body() createUserDto: CreateUserDto): User {
+    return this.appService.createUser(createUserDto);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.model';
 
 @Injectable()
 export class AppService {
+  private users: User[] = [];
+  private id = 1;
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  getUsers(): { id: string; name: string; age: number }[] {
+  getUsers(): { id: number; name: string; age: number }[] {
     const users = [
-      { id: '01', name: '山田太郎', age: 24 },
-      { id: '02', name: '佐藤次郎', age: 25 },
-      { id: '03', name: '伊藤三郎', age: 26 },
-      { id: '04', name: '高橋四郎', age: 27 },
-      { id: '05', name: '茂野五郎', age: 28 },
+      { id: 1, name: '山田太郎', age: 24 },
+      { id: 2, name: '佐藤次郎', age: 25 },
+      { id: 3, name: '伊藤三郎', age: 26 },
+      { id: 4, name: '高橋四郎', age: 27 },
+      { id: 5, name: '茂野五郎', age: 28 },
     ];
     return users;
+  }
+
+  createUser(createTodoDto: CreateUserDto): User {
+    const { name, age } = createTodoDto;
+    const user = {
+      id: this.id++,
+      name,
+      age,
+    };
+    this.users.push(user);
+    return user;
   }
 }
