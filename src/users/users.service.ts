50fbb1e7f0b76c74284baf1c6995f9c3eb01408a
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-25 22:18:34 +0900

    ✨ 前ユーザー取得処理を仮データ状態から本実装に修正

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,15 +7,8 @@ export class UsersService {
   private users: User[] = [];
   private id = 1;
 
-  getUsers(): { id: number; name: string; age: number }[] {
-    const users = [
-      { id: 1, name: '山田太郎', age: 24 },
-      { id: 2, name: '佐藤次郎', age: 25 },
-      { id: 3, name: '伊藤三郎', age: 26 },
-      { id: 4, name: '高橋四郎', age: 27 },
-      { id: 5, name: '茂野五郎', age: 28 },
-    ];
-    return users;
+  getUsers(): User[] {
+    return this.users;
   }
 
   getUserById(id: number): User {
